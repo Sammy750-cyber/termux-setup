6d20cd3f8416f46b2945c58235ea74aed8ecb31d
SCWhack <126519976+Sammy750-cyber@users.noreply.github.com>
2024-07-30 22:45:58 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,17 @@
+# TERMUX SET-UP TOOL
+<p align="center"><img src="screenshorts/setupimg.PNG">
+
+<p>Tool for basic and  full termux installations. </p>
+<p>Setup our termux with just one click. Please follow and give star if you use this tool.</p>
+
+## USAGE
+```python 
+pkg update
+apt upgrade
+pkg install python
+pip install git
+git clone https://github.com/sammy750-cyber/termux-setup
+cd termux-setup/
+python setup.py
+```
+#### THANKS FOR USING MY TOOL
---
+++ setup.py
@@ -0,0 +1,225 @@
+import random
+import time
+import sys
+import os
+os.system("xdg-open https://github.com/sammy750-cyber")
+class color:
+    RED='\033[91m'
+    YELLOW='\033[93m'
+    UNDERLINE='\033[4m'
+    GREEN='\033[92m'
+    BLUE='\033[94m'
+    PURPLE='\033[95m'
+    BOLD='\033[1m'
+    DARKCYAN='\033[36m'
+    END='\033[0m'
+
+def basic():
+  print("\n\nINSTALLING START IN 3 SEC...\n\n")
+  time.sleep(3)
+  os.system("pkg update")
+  os.system("pkg upgrade")
+  os.system("pkg install python -y")
+  os.system("pkg install python2 -y")
+  os.system("pkg install python3")
+  os.system("apt install tmux")
+  os.system("pip install requests")
+  os.system("pip2 install requests")
+  os.system("pip install mechanize")
+  os.system("pip2 install mechanize")
+  os.system("pip install bs4")
+  os.system("pip2 install bs4")
+  os.system("pip install rich")
+  os.system("pkg install ruby -y")
+  os.system("gem install lolcat")
+  os.system("pkg install python-pip")
+  os.system("pip install yt_dlp")
+  os.system("pip install php")
+  os.system("pip2 install php")
+  os.system("pip install lolcat")
+  os.system("pip install futures")
+  os.system("pip2 install futures")
+  os.system("pkg install nano")
+  os.system("pip install espeak")
+  os.system("clear")
+  print("""\033[1;32m
+ _______  _______ _________          _______ 
+(  ____ \(  ____ \\__   __/|\     /|(  ____ )
+| (    \/| (    \/   ) (   | )   ( || (    )|
+| (_____ | (__       | |   | |   | || (____)|
+(_____  )|  __)      | |   | |   | ||  _____)
+      ) || (         | |   | |   | || (      
+/\____) || (____/\   | |   | (___) || )      
+\_______)(_______/   )_(   (_______)|/       
+                                                                                 
+  ____   ___  _   _ _____ 
+ |  _ \ / _ \| \ | | ____|
+ | | | | | | |  \| |  _|  
+ | |_| | |_| | |\  | |___ 
+ |____/ \___/|_| \_|_____|
+                          
+
+
+TNX FOR USING MY TOOLS..
+
+
+
+""")
+  os.system("espeak -a 500 'Setup Done'")
+  
+def full():
+  print("\n\nSETUP START IN 3 SEC...\n\n")
+  time.sleep(3)
+  os.system("pkg update")
+  os.system("pkg upgrade")
+  os.system("pkg install python")
+  os.system("pkg install python2")
+  os.system("pkg install python3")
+  os.system("pkg install wget")
+  os.system("pkg install python-pip")
+  os.system("pkg install fish")
+  os.system("pkg install ruby")
+  os.system("pkg install help")
+  os.system("pkg install dnsutils  ")
+  os.system("pkg install php")
+  os.system("pkg install perl")
+  os.system("pkg install lua")
+  os.system("pkg install parallel")
+  os.system("pkg install nmap")
+  os.system("pkg install bash")
+  os.system("pkg install clang")
+  os.system("pkg install nano")
+  os.system("pkg install w3m")
+  os.system("pkg install hydra")
+  os.system("pkg install figlet")
+  os.system("pkg install cowsay")
+  os.system("pkg install curl")
+  os.system("pkg install tar")
+  os.system("pkg install zip")
+  os.system("pkg install unzip")
+  os.system("pkg install tor")
+  os.system("pkg install net-tools")
+  os.system("pkg install sudo")
+  os.system("pkg install wireshark")
+  os.system("apt install tmux")
+  os.system("pkg install crunch")
+  os.system("pkg install wgetrc")
+  os.system("pkg install wgetrc")
+  os.system("pkg install wcalc")
+  os.system("pkg install openssl")
+  os.system("pkg install openssl-tool")
+  os.system("pkg install bmon")
+  os.system("pkg install vpn")
+  os.system("pkg install unrar")
+  os.system("pkg install toilet")
+  os.system("pkg install proot")
+  os.system("pkg install net-tools")
+  os.system("pkg install vim")
+  os.system("pkg install vim-python")
+  os.system("pkg install ired")
+  os.system("pkg install goaccess")
+  os.system("pkg install golang")
+  os.system("pkg install kibi")
+  os.system("pkg install tsu")
+  os.system("pkg install mtools")
+  os.system("pkg install file")
+  os.system("pkg install vis")
+  os.system("pkg install pass")
+  os.system("pkg install pick")
+  os.system("pkg install chroot")
+  os.system("pkg install macchanger")
+  os.system("pkg install ninja")
+  os.system("pkg install elixir")
+  os.system("pkg install swift")
+  os.system("pkg install xmlstarlet")
+  os.system("pkg install fakeroot")
+  os.system("pkg install texinfo")
+  os.system("pkg install netcat")
+  os.system("pkg install wren")
+  os.system("pkg install gatling")
+  os.system("pkg install cvs")
+  os.system("pkg install ffmpeg")
+  os.system("pkg install screen")
+  os.system("pkg install neofetch")
+  os.system("pkg install mariadb")
+  os.system("pkg install picolisp")
+  os.system("pkg install cmatrix")
+  os.system("pkg install dropbear")
+  os.system("pkg install openssh")
+  os.system("pkg install python-pip")
+  os.system("pip install yt_dlp")
+  os.system("pip2 install wget")
+  os.system("pip install bs4")
+  os.system("pip2 install bs4")
+  os.system("pip install requests")
+  os.system("pip2 install requests")
+  os.system("pip install mechanize")
+  os.system("pip2 install mechanize")
+  os.system("pip install php")
+  os.system("pip2 install php")
+  os.system("gem install lolcat")
+  os.system("pip install espeak")
+  os.system("clear")
+  print("""\033[1;32m
+ _______  _______ _________          _______ 
+(  ____ \(  ____ \\__   __/ |\     /|(  ____ )
+| (    \/| (    \/   ) (   | )   ( || (    )|
+| (_____ | (__       | |   | |   | || (____)|
+(_____  )|  __)      | |   | |   | ||  _____)
+      ) || (         | |   | |   | || (      
+/\____) || (____/\   | |   | (___) || )      
+\_______)(_______/   )_(   (_______)|/       
+                                             
+ ____   ___  _   _ _____ 
+|  _ \ / _ \| \ | | ____|
+| | | | | | |  \| |  _|  
+| |_| | |_| | |\  | |___ 
+|____/ \___/|_| \_|_____|
+                                    
+TNX FOR USING MY TOOLS..
+	
+	
+""")
+  os.system("espeak -a 500 'Setup Done'")
+
+
+
+logo= (f"""{color.GREEN}
+████████╗███╗   ███╗██╗  ██╗     ███████╗███████╗████████╗██╗   ██╗██████╗ 
+╚══██╔══╝████╗ ████║╚██╗██╔╝     ██╔════╝██╔════╝╚══██╔══╝██║   ██║██╔══██╗
+   ██║   ██╔████╔██║ ╚███╔╝█████╗███████╗█████╗     ██║   ██║   ██║██████╔╝
+   ██║   ██║╚██╔╝██║ ██╔██╗╚════╝╚════██║██╔══╝     ██║   ██║   ██║██╔═══╝ 
+   ██║   ██║ ╚═╝ ██║██╔╝ ██╗     ███████║███████╗   ██║   ╚██████╔╝██║     
+   ╚═╝   ╚═╝     ╚═╝╚═╝  ╚═╝     ╚══════╝╚══════╝   ╚═╝    ╚═════╝ ╚═╝   {color.END}
+{color.BOLD}{color.BLUE}==============================================={color.END}
+{color.BLUE}[{color.END}{color.BOLD}{color.PURPLE}={color.END}{color.BLUE}]{color.END}  {color.YELLOW}DEVELOPER{color.END}     {color.BOLD}:{color.END}  {color.GREEN} SCWhack {color.END}
+{color.BLUE}[{color.END}{color.BOLD}{color.PURPLE}={color.END}{color.BLUE}]{color.END}  {color.YELLOW}GITHUB{color.END}        {color.BOLD}:{color.END}  {color.GREEN} Sammy750-cyber {color.END}
+{color.BLUE}[{color.END}{color.BOLD}{color.PURPLE}={color.END}{color.BLUE}]{color.END}  {color.YELLOW}VERSION{color.END}       {color.BOLD}:{color.END}  {color.GREEN} 1.0 {color.END}
+{color.BLUE}[{color.END}{color.BOLD}{color.PURPLE}={color.END}{color.BLUE}]{color.END}  {color.YELLOW}TELEGRAM{color.END}      {color.BOLD}:{color.END}  {color.GREEN} NOGROUPYET {color.END}
+{color.BLUE}[{color.END}{color.BOLD}{color.PURPLE}={color.END}{color.BLUE}]{color.END}  {color.YELLOW}TOOL'S NAME{color.END}   {color.BOLD}:{color.END}  {color.GREEN} TMX SETUP {color.END}
+{color.BLUE}[{color.END}{color.BOLD}{color.PURPLE}={color.END}{color.BLUE}]{color.END}  {color.YELLOW}STATUS{color.END}        {color.BOLD}:{color.END}  {color.GREEN} FREE {color.END}
+{color.BOLD}{color.BLUE}==============================================={color.END}
+{color.BLUE}[{color.END}{color.PURPLE}01/A{color.END}{color.BLUE}]{color.END} {color.GREEN} Basic Setup {color.END}
+{color.BLUE}[{color.END}{color.PURPLE}02/B{color.END}{color.BLUE}]{color.END} {color.GREEN} Full Setup {color.END}
+{color.BLUE}[{color.END}{color.PURPLE}03/C{color.END}{color.BLUE}]{color.END} {color.GREEN} Report Bugss {color.END}
+{color.BLUE}[{color.END}{color.PURPLE}{color.RED}04/X{color.END}{color.BLUE}]{color.END} {color.GREEN} {color.RED}Exit Programme {color.END}
+{color.BOLD}{color.BLUE}==============================================={color.END}
+""")
+
+for i in logo:
+    sys.stdout.write(i)
+    sys.stdout.flush()
+    time.sleep(0.0001) 
+user_input=input(f"{color.BLUE}{color.BOLD}[{color.GREEN}>{color.BLUE}]{color.END} {color.GREEN}CHOOSE: ")
+user_input=user_input.replace(' ','')
+if user_input == "01" or user_input == "a" or user_input == "A" or user_input == "1":
+   basic()
+elif user_input == "02" or user_input == "b" or user_input == "B" or user_input == "2":
+   full()
+elif user_input == "03" or user_input == "c" or user_input == "C" or user_input == "3":
+   os.system("xdg-open https://github.com/sammy750-cyber")
+elif user_input ==  "04" or user_input == "x" or user_input == "X" or user_input == "4":
+   sys.exit()
+else:
+   print(f"{color.RED}Invalid Input!!! {color.END}")
+   sys.exit()
